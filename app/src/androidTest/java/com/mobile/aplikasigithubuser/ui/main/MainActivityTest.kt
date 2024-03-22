52fee9146316369f2cb1f883ec6d0567914e8714
LorenzoLiu75 <lorenzoliu797@gmail.com>
2024-03-22 14:00:36 +0700

    added ui test

---
+++ app/src/androidTest/java/com/mobile/aplikasigithubuser/ui/main/MainActivityTest.kt
@@ -0,0 +1,32 @@
+package com.mobile.aplikasigithubuser.ui.main
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.mobile.aplikasigithubuser.R
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class MainActivityTest {
+
+    @Before
+    fun setup() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun testItemClick() {
+
+        onView(withId(R.id.listUsers)).perform(actionOnItemAtPosition<GithubAdapter.MyViewHolder>(0, click()))
+
+        onView(withId(R.id.detailUserLayout)).check(matches(isDisplayed()))
+    }
+}
